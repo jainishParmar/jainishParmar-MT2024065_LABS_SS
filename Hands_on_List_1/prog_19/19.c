2d4876ca6d0059171b079612b9404ae93316c97a
jainish_parmar <Jainishkumar.Parmar@iiitb.ac.in>
2024-08-22 09:25:42 +0530

    1st it

--- Hands_on_List_1/prog_19/19.c
+++ Hands_on_List_1/prog_19/19.c
@@ -1,6 +1,30 @@
-#include<stdio.h>
+// jainish parmar
+// MT2024065
+// Write a program to find out time taken to execute getpid system call. Use time stamp counter.
 
-int main(){
-    printf("hello");
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+unsigned long long rdtsc()
+{
+    unsigned long long dst;
+    __asm__ __volatile__("rdtsc" : "=A"(dst));
+    return dst;
+}
+
+int main()
+{
+    int pid = fork();
+    if (pid == 0)
+    {
+        long long int start, end;
+
+        start = rdtsc();
+        printf("process_id:= %d\n", getpid());
+        end = rdtsc();
+
+        printf("time taken is : %llu\n", end - start);
+    }
     return 0;
 }
