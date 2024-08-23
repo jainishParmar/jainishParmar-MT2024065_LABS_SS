27eb4ab309353d1f36f12e61617e2a3560aa265f
jainish_parmar <Jainishkumar.Parmar@iiitb.ac.in>
2024-08-23 15:51:04 +0530

    1st it

---
+++ Hands_on_List_1/prog_22/22.c
@@ -0,0 +1,30 @@
+//jainish parmar
+//MT2024065
+//Write a program, open a file, call fork, and then write to the file by both the child as well as the parent processes. Check output of the file
+
+
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<string.h>
+#include <sys/wait.h>
+
+int main(){
+    int n=5;
+    int fd=open("a.txt",O_RDWR);
+    pid_t pid=fork();
+    while(n--){
+        if(pid==0){
+            //child;
+            write(fd,"child\n",strlen("child\n"));
+            sleep(1);
+        }else{
+            //parent;
+             write(fd,"parent\n",strlen("parent\n"));
+             sleep(2);
+        }
+    }
+    wait(0);
+    close(fd);
+    return 0;
+}
