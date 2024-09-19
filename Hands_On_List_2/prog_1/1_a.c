de403092901aa3faf7a6ab917b79c9a7be575313
jainish_parmar <Jainishkumar.Parmar@iiitb.ac.in>
2024-09-19 13:23:49 +0530

    1st_prog

---
+++ Hands_On_List_2/prog_1/1_a.c
@@ -0,0 +1,45 @@
+//jainish parmar
+//MT2024065
+/*
+1.  Write a separate program (for each time domain) to set a interval timer in 10sec and 10micro second        
+a. ITIMER_REAL        b. ITIMER_VIRTUAL        c. ITIMER_PROF
+*/
+
+#include <sys/time.h> 
+#include <signal.h>   
+#include <unistd.h>   
+#include <stdio.h>    
+
+void main(int argc, char *argv[])
+{
+    int timerStatus; 
+
+    struct itimerval timer;
+
+    if (argc != 2)
+    {
+        printf("Pass 1 for 10s timer or 2 for 10ms timer\n");
+        _exit(0);
+    }
+
+    if ((int)(*argv[1] - 48) == 1)
+    {
+        timer.it_interval.tv_sec = 0;
+        timer.it_interval.tv_usec = 0;
+        timer.it_value.tv_sec = 10;
+        timer.it_value.tv_usec = 0;
+    }
+    else if ((int)(*argv[1] - 48) == 2)
+    {
+        timer.it_interval.tv_sec = 0;
+        timer.it_interval.tv_usec = 0;
+        timer.it_value.tv_sec = 0;
+        timer.it_value.tv_usec = 10;
+    }
+
+    timerStatus = setitimer(ITIMER_REAL, &timer, 0);
+    if (timerStatus == -1)
+        perror("Error while setting an interval timer!");
+
+    while(1);
+}
