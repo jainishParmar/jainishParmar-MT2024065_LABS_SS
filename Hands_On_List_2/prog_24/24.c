dd7613746c0dbde4116cd416972a2f22d495cf8b
jainish_parmar <Jainishkumar.Parmar@iiitb.ac.in>
2024-09-20 19:14:57 +0530

    prog_25 done

---
+++ Hands_On_List_2/prog_24/24.c
@@ -0,0 +1,27 @@
+//Write a program to create a message queue and print the key and message queue id.
+
+#include <sys/types.h>
+#include <sys/ipc.h>   
+#include <sys/msg.h>   
+#include <stdio.h>     
+#include <unistd.h>    
+#include <errno.h>    
+void main()
+{
+    key_t queueKey;      
+    int queueIdentifier; 
+    queueKey = ftok(".", 1);
+    if (queueKey == -1)
+    {
+        perror("Error while computing key!");
+        _exit(0);
+    }
+    queueIdentifier = msgget(queueKey, IPC_CREAT | IPC_EXCL | 0700);
+    if (queueIdentifier == -1)
+    {
+        perror("Error while creating Message Queue!");
+        _exit(0);
+    }
+    printf("Key: %d\n", queueKey);
+    printf("Message Queue Identifier: %d\n", queueIdentifier);
+}
