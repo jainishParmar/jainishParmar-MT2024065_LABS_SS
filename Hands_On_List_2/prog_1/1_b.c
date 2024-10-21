4a75023fa97d605e5573c046b14258787c508fad
jainish_parmar <Jainishkumar.Parmar@iiitb.ac.in>
2024-10-21 21:17:48 +0530

    bug fixed

--- Hands_On_List_2/prog_1/1_b.c
+++ Hands_On_List_2/prog_1/1_b.c
@@ -16,26 +16,10 @@ void main(int argc, char *argv[])
 
     struct itimerval timer;
 
-    if (argc != 2)
-    {
-        printf("Pass 1 for 10s timer or 2 for 10ms timer\n");
-        _exit(0);
-    }
-
-    if ((int)(*argv[1] - 48) == 1)
-    {
-        timer.it_interval.tv_sec = 0;
-        timer.it_interval.tv_usec = 0;
-        timer.it_value.tv_sec = 10;
-        timer.it_value.tv_usec = 0;
-    }
-    else if ((int)(*argv[1] - 48) == 2)
-    {
-        timer.it_interval.tv_sec = 0;
-        timer.it_interval.tv_usec = 0;
-        timer.it_value.tv_sec = 0;
-        timer.it_value.tv_usec = 10;
-    }
+    timer.it_interval.tv_sec = 0;
+    timer.it_interval.tv_usec = 0;
+    timer.it_value.tv_sec = 10;
+    timer.it_value.tv_usec = 10;
 
     timerStatus = setitimer(ITIMER_VIRTUAL, &timer, 0);
     if (timerStatus == -1)
